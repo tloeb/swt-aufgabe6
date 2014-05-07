dd5b70bd588fa13db01904aa9c2105db179a36b2
Tobias Löb <tobias_loeb@web.de>
2014-05-07 17:59:37 +0200

    Add increasePrices() Test

--- src/de/ostfalia/swt/aufgabe6/XmlIoImpl.java
+++ src/de/ostfalia/swt/aufgabe6/XmlIoImpl.java
@@ -9,64 +9,70 @@ import java.util.List;
 
 import org.jdom2.Element;
 import org.jdom2.Document;
+import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 import org.jdom2.input.SAXBuilder;
 
 /**
  * Created by Tobias on 07.05.2014.
  */
-public class XmlIoImpl implements XmlIo{
+public class XmlIoImpl implements XmlIo {
 
-    @Override
-    public void writeProducts(List<Product> products, OutputStream out) throws IOException {
-        Element xmlProducts = new Element("products");
-        Document doc = new Document(xmlProducts);
-        for(Product p : products) {
-            Element product = new Element("product");
-            product.addContent(new Element("id").setText(p.getId().toString()));
-            product.addContent(new Element("description").setText(p.getDescription()));
-            product.addContent(new Element("price").setText(p.getPrice().toString()));
-            doc.getRootElement().addContent(product);
-        }
-        XMLOutputter xmlOutput = new XMLOutputter();
-        xmlOutput.output(doc, out);
-    }
+	@Override
+	public void writeProducts(List<Product> products, OutputStream out)
+			throws IOException {
+		Element xmlProducts = new Element("products");
+		Document doc = new Document(xmlProducts);
+		for (Product p : products) {
+			Element product = new Element("product");
+			product.addContent(new Element("id").setText(p.getId().toString()));
+			product.addContent(new Element("description").setText(p
+					.getDescription()));
+			product.addContent(new Element("price").setText(p.getPrice()
+					.toString()));
+			doc.getRootElement().addContent(product);
+		}
+		XMLOutputter xmlOutput = new XMLOutputter();
+		xmlOutput.setFormat(Format.getPrettyFormat());
+		xmlOutput.output(doc, out);
+	}
 
-    @Override
-    public List<Product> readProducts(InputStream in) throws Exception {
-        List<Product> productList = new ArrayList<Product>();
-        SAXBuilder builder = new SAXBuilder();
-        Document document = builder.build(in);
-        Element rootNode = document.getRootElement();
-        List xmlList = rootNode.getChildren("product");
-        for( Object o : xmlList) {
-            Element e = (Element) o ;
-            int id = Integer.parseInt(e.getChildText("id"));
-            String desc = e.getChildText("description");
-            BigDecimal price = new BigDecimal(e.getChildText("price"));
-            Product p = new Product(id, desc, price);
-            productList.add(p);
-        }
-        return productList;
-    }
+	@Override
+	public List<Product> readProducts(InputStream in) throws Exception {
+		List<Product> productList = new ArrayList<Product>();
+		SAXBuilder builder = new SAXBuilder();
+		Document document = builder.build(in);
+		Element rootNode = document.getRootElement();
+		List xmlList = rootNode.getChildren("product");
+		for (Object o : xmlList) {
+			Element e = (Element) o;
+			int id = Integer.parseInt(e.getChildText("id"));
+			String desc = e.getChildText("description");
+			BigDecimal price = new BigDecimal(e.getChildText("price"));
+			Product p = new Product(id, desc, price);
+			productList.add(p);
+		}
+		return productList;
+	}
 
-    @Override
-    public Product getProduct(InputStream in, Integer id) throws Exception {
-    	List<Product> productList = readProducts(in);
-    	for (Product product : productList) {
+	@Override
+	public Product getProduct(InputStream in, Integer id) throws Exception {
+		List<Product> productList = readProducts(in);
+		for (Product product : productList) {
 			if (product.getId() == id) {
 				return product;
 			}
 		}
-        return null;
-    }
+		return null;
+	}
 
-    @Override
-    public void increasePrices(InputStream in, OutputStream out, BigDecimal percent) throws Exception {
-    List<Product> productList = readProducts(in);
-        for (Product p : productList) {
-            p.setPrice(percent.multiply(p.getPrice()));
-        }
-    writeProducts(productList, out);
-    }
+	@Override
+	public void increasePrices(InputStream in, OutputStream out,
+			BigDecimal percent) throws Exception {
+		List<Product> productList = readProducts(in);
+		for (Product p : productList) {
+			p.setPrice(percent.multiply(p.getPrice()));
+		}
+		writeProducts(productList, out);
+	}
 }
--- src/de/ostfalia/swt/aufgabe6/XmlIoImplTest.java
+++ src/de/ostfalia/swt/aufgabe6/XmlIoImplTest.java
@@ -3,17 +3,33 @@ import static org.junit.Assert.*;
 
 import org.junit.*;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
  * Created by Tobias on 07.05.2014.
  */
 public class XmlIoImplTest {
-//    @Test
-    void testWriteProducts() {
+    @Test
+    public void testWriteProducts() {
+    	try {
+    		File f = new File("products_test.xml");
+        	OutputStream out = new FileOutputStream(f);
+//    		OutputStream out = System.out;
+        	XmlIo tmp = new XmlIoImpl();
+        	InputStream inputstream = new FileInputStream("products.xml");
+            List<Product> pList = tmp.readProducts(inputstream);
+            tmp.writeProducts(pList, out);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 
+    	
     }
     @Test
     public void testReadProducts() {
@@ -50,8 +66,18 @@ public class XmlIoImplTest {
 		}
 
     }
-//    @Test
-    void testIncreasePrices() {
+    @Test
+    public void testIncreasePrices() {
+    	try {
+    		File f = new File("products_test_price.xml");
+    		OutputStream out = System.out;
+        	XmlIo tmp = new XmlIoImpl();
+        	InputStream in1 = new FileInputStream("products.xml");
+            System.out.println("---------");
+            tmp.increasePrices(in1, out, new BigDecimal(2));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
     }
 }
